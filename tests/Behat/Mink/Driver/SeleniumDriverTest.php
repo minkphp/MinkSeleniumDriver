9d16d11220e43be7647aaeeedf8257685a3b55c1
Christophe Coevoet <stof@notk.org>
2014-02-04 13:51:22 +0100

    Removed the otherMouseEvents tests
    This is not needed anymore as the tests have been split into different
    methods in the Mink driver testsuite

--- tests/Behat/Mink/Driver/SeleniumDriverTest.php
+++ tests/Behat/Mink/Driver/SeleniumDriverTest.php
@@ -22,30 +22,6 @@ class SeleniumDriverTest extends JavascriptDriverTest
         );
     }
 
-    public function testMouseEvents()
-    {
-        $this->markTestIncomplete('testMouseEvents cannot be tested fully for SeleniumDriver. Supported events are currently tested in testOtherMouseEvents');
-    }
-
-    public function testOtherMouseEvents()
-    {
-        // focus, blur and right click is not supported currently
-        $this->getSession()->visit($this->pathTo('/js_test.php'));
-
-        $clicker = $this->getSession()->getPage()->find('css', '.elements div#clicker');
-
-        $this->assertEquals('not clicked', $clicker->getText());
-
-        $clicker->click();
-        $this->assertEquals('single clicked', $clicker->getText());
-
-        $clicker->doubleClick();
-        $this->assertEquals('double clicked', $clicker->getText());
-
-        $clicker->mouseOver();
-        $this->assertEquals('mouse overed', $clicker->getText());
-    }
-
     public function testIssue193()
     {
         $this->markTestSkipped('Selenium1 doesn\'t handle selects without values');
